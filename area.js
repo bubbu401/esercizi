b3731a1c1a95ef5ea88e6a31bd8e4e838995981f
stefano sette <stefnosette5451@gmail.com>
2026-05-15 17:58:26 +0200

    ho sistemato il codice

--- area.js
+++ area.js
@@ -5,7 +5,7 @@ let ar = process.argv.slice(2);
 
 
 
-if (ar.length < 1){
+if (ar.length < 1 || ar.length > 3){
 console.log("per calcolare l area: <nome poligono> <numero> <numero>");
 console.log("es:quadrato 5 oppure rettangolo 1 5");
 process.exit(1);
@@ -20,8 +20,13 @@ if (poligono ==="quadrato"){
 	console.log("area e");
 	risultato = area.areaquadrato(b);
 }else if (poligono === "rettangolo"){
+	if (ar.length !== 3){
+	console.log("per il rettangolo e necessario inserire due parametri base e altezza");
+	process.exit(1);
+	}else{
 	console.log("area e");
 	risultato =  area.arearettangolo(b,h);
+	}
 }else if (poligono === "cerchio"){
 	console.log("area e");
 	risultato = area.areacerchio(b);
